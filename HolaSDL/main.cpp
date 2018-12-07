56e8ead0004b19adb781b746656b4c7fcbb8dff8
IgnOry <iory@ucm.es>
2018-12-07 20:49:48 +0100

    Base para la práctica 3
    Intento de instalar TTF por enesima vez, sigue sin funcionar :)

--- HolaSDL/main.cpp
+++ HolaSDL/main.cpp
@@ -3,38 +3,16 @@
 #include "SDL_image.h"
 #include "checkML.h"
 #include <iostream>
-#include "Game.h"
+//#include <SDL_ttf.h> // Puede variar dependiendo de la instalación
 
 using namespace std;
-
 using uint = unsigned int;
 
-int menu()
-{
-	int x;
-
-	cout << "(0) - Partida normal" << endl;
-	cout << "(1) - Cargar partida" << endl;
-	cout << "(2) - Mejores jugadores" << endl;
-
-	cin >> x;
-	return x;
-}
 
 int main(int argc, char* argv[]){
-	
-	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-
-	int x = menu();
-
-	try {
-		Game* game = new Game(x);
-		game->run();
-		delete game;
-	}
-	catch (exception e)
-	{
-		cout << e.what();
-	}
+	cout << "Hello World" << endl;
+	cout << "Hello World" << endl;
+	int a;
+	cin >> a;
 	return 0;
 }
